1d87c2f390f3b69aec59efd0098710f9f094a12c
Tomasz Gorochowik <tgorochowik@gmail.com>
2021-04-27 19:09:18 +0200

    Add support for special (non-regular char) entries

--- pokestrings.py
+++ pokestrings.py
@@ -8,6 +8,22 @@ pokecodes = list(range(0x7f, 0xc0)) + list(range(0xe0, 0x100))
 
 decodings = dict(zip(pokecodes, pokechars))
 
+# add special entries
+special = {
+    # text control codes
+    0x4a: "PkMn",
+    0x54: "Poke",
+    0x56: "...",
+    0x5b: "PC",
+    0x5c: "TM",
+    0x5d: "TRAINER",
+    0x5e: "ROCKET",
+    0x6d: ":",
+}
+decodings.update(special)
+
+pokecodes = pokecodes + list(special.keys())
+
 
 def decode(data):
     # discard large lists of 0xff (encoding for '9')
